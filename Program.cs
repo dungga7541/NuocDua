88ee8f1e48680da71bd536ce5fe732d613289fdb
dungga7541 <54361574+dungga7541@users.noreply.github.com>
2020-06-28 10:02:52 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace onthiCC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CAU1
+            //cau1 bt1 = new cau1();
+            //bt1.BT1();
+            //cau1 bt3 = new cau1();
+            //bt3.BT3();
+            //cau1 bt5 = new cau1();
+            //bt5.BT5();
+            //cau1 bt7 = new cau1();
+            //bt7.BT7();
+            //cau1 bt9 = new cau1();
+            //bt9.BT9();
+            //cau1 bt11 = new cau1();
+            //bt11.BT11();
+            //cau1 bt13 = new cau1();
+            //bt13.BT13();
+            //cau1 bt15 = new cau1();
+            //bt15.BT15();
+            //cau1 bt17 = new cau1();
+            //bt17.BT17();
+            //cau1 bt19 = new cau1();
+            //bt19.BT19();
+            //cau1 bt21 = new cau1();
+            //bt21.BT21();
+            //cau1 bt23 = new cau1();
+            //bt23.BT23();
+            //cau1 bt25 = new cau1();
+            //bt25.BT25();
+            //cau1 bt27 = new cau1();
+            //bt27.BT27();
+            //cau1 bt29 = new cau1();
+            //bt29.BT29();
+            //cau1 bt31 = new cau1();
+            //bt31.BT31();
+            //cau1 bt33 = new cau1();
+            //bt33.BT33();
+            //cau1 bt35 = new cau1();
+            //bt35.BT35();
+            //cau1 bt37 = new cau1();
+            //bt37.BT37();
+            //cau1 bt39 = new cau1();
+            //bt39.BT39();
+        }
+    }
+}
